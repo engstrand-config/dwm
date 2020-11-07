f5fe3978aece4b56a59a9fa7380ca4977bd03225
Fredrik Engstrand <fredrik@engstrand.nu>
2020-11-07 20:47:30 +0100

    patch: load colors from xresources

--- config.h
+++ config.h
@@ -1,14 +1,21 @@
 #include <X11/XF86keysym.h>
 
 /* general */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
 
 /* appearance */
-static unsigned int baralpha        = 225;
+static char font[]                  = "JetBrains Mono NL:style=Bold:size=9:antialias=true:autohint=true";
+static char *fonts[] = { font, "monospace:size=10" };
+
+static unsigned int baralpha        = 225;      /* transparency of bar */
+static unsigned int barheight       = 20;       /* height of the bar */
+static unsigned int borderpx        = 1;        /* border pixel of windows */
+static unsigned int gappih          = 10;       /* horiz inner gap between windows */
+static unsigned int gappiv          = 10;       /* vert inner gap between windows */
+static unsigned int gappoh          = 10;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov          = 10;       /* vert outer gap between windows and screen edge */
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -181,6 +188,27 @@ static Button buttons[] = {
   { ClkTagBar,            0,              Button3,        toggleview,     {0}                },
   { ClkTagBar,            MODKEY,         Button1,        tag,            {0}                },
   { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0}                },
+};
+
+/* Xresources preferences to load at startup */
+ResourcePref resources[] = {
+		{ "font",                       STRING,  &font },
+		{ "barHeight",                  INTEGER, &barheight },
+		{ "barAlpha",                   INTEGER, &baralpha },
+		{ "background",                 STRING,  &normbgcolor },
+		{ "foreground",                 STRING,  &normfgcolor },
+		{ "borderColor",                STRING,  &normbordercolor },
+		{ "selectedBackground",         STRING,  &selbgcolor },
+		{ "selectedForeground",         STRING,  &selfgcolor },
+		{ "selectedBorderColor",        STRING,  &selbordercolor },
+		{ "titleBackground",            STRING,  &titlebgcolor },
+		{ "titleForeground",            STRING,  &titlefgcolor },
+		{ "titleBorderColor",           STRING,  &titlebordercolor },
+		{ "borderSize",          	      INTEGER, &borderpx },
+		{ "gapsInner",          	      INTEGER, &gappih },
+		{ "gapsInner",          	      INTEGER, &gappiv },
+		{ "gapsOuter",          	      INTEGER, &gappoh },
+		{ "gapsOuter",          	      INTEGER, &gappov },
 };
 
 /* signal definitions */
